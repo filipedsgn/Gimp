b4c97a0e0ac49a3f1781f6b61ec14551bd18ef17
FilipeDSGN <filipe.dsgn@hotmail.com>
2018-04-29 00:18:13 -0300

    .

--- setup.sh
+++ setup.sh
@@ -4,6 +4,6 @@ mkdir ~/Documentos/Gimp
 mv ./Gimp/nomes.csv ~/Documentos/Gimp/nomes.csv
 mv ./Gimp/executar.sh ~/Documentos/Gimp/executar.sh
 mv ./Gimp/modelo.xcf ~/Documentos/Gimp/modelo.xcf
-mv ./Scripts/*.py ~/.gimp*/plug-ins/
-rm -rf ../Gimp
+mv ./Gimp/Scripts/*.py ~/.gimp*/plug-ins/
+rm -rf ./Gimp
 zenity --info --text="Instalação Concluída"
